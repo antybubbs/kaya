358a30bcedc735f61df040790a91060d3ca5eabb
antybubbs <143219954+antybubbs@users.noreply.github.com>
2026-06-19 11:46:00 +0100

    Remove locale environment variables
    Removed unnecessary locale environment variables from the configuration.

--- scripts/homelab-remote-manager.cjs
+++ scripts/homelab-remote-manager.cjs
@@ -10,14 +10,6 @@ const UTF8_ENV = {
   FORCE_COLOR: "1",
   CLICOLOR: "1",
   CLICOLOR_FORCE: "1",
-  LANG: "en_US.UTF-8",
-  LC_ALL: "en_US.UTF-8",
-  LC_CTYPE: "en_US.UTF-8",
-  LC_MESSAGES: "en_US.UTF-8",
-  LC_MONETARY: "en_US.UTF-8",
-  LC_NUMERIC: "en_US.UTF-8",
-  LC_TIME: "en_US.UTF-8",
-  LC_COLLATE: "en_US.UTF-8",
 };
 
 function send(ws, payload) {
